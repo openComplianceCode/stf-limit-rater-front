6b7d64eab6c8d6af9bc203fa3cc7232cbfac4004
郑志鹏 <zhengzhipeng4@huawei.com>
2022-05-20 00:01:04 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  plugins: ["vuetify"],
+
+  extends: "vuetify",
+
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off",
+    // https://github.com/babel/babel-eslint/issues/681#issuecomment-420663038
+    "template-curly-spacing": "off",
+    indent: "off",
+    //
+    "no-unused-vars": "warn",
+    "vue/script-indent": [
+      "error",
+      2,
+      {
+        baseIndent: 0,
+        switchCase: 0,
+        ignores: []
+      }
+    ],
+    "vue/max-attributes-per-line": [
+      "off",
+      {
+        singleline: {
+          max: 1
+        },
+        multiline: {
+          max: 1
+        }
+      }
+    ]
+  },
+
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
+};
